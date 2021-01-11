6a61519f4718519ba284a6fb23d1b0d6b0d8c3da
anmteh <anmteh@gmail.com>
2021-01-11 18:06:48 +0300

    added and styled draft controls

--- src/components/DraftControls.tsx
+++ src/components/DraftControls.tsx
@@ -5,16 +5,41 @@ import CloseIcon from '@material-ui/icons/Close'
 
 const StyledDraft = styled.div`
 	display: flex;
-	justify-content: space-between;
+	justify-content: space-around;
+	margin-top: 1rem;
 	height: 20%;
 	width: 100%;
+	& .right {
+		background-color: rgba(36, 199, 90, 0.8);
+		box-shadow: 0px 0px 10px rgba(36, 199, 90, 0.8);
+	}
+	& .wrong {
+		box-shadow: 0 0 10px rgba(232, 28, 75, 0.8);
+		background-color: rgba(232, 28, 75, 0.8);
+	}
+`
+
+const ControlButtonContainer = styled.div`
+	border-radius: 50%;
+	height: 4rem;
+	width: 4rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 3rem;
+	color: white;
+	cursor: pointer;
 `
 
 export default function DraftControls() {
 	return (
 		<StyledDraft>
-			<DoneIcon />
-			<CloseIcon />
+			<ControlButtonContainer className='right'>
+				<DoneIcon fontSize='inherit' color='inherit' />
+			</ControlButtonContainer>
+			<ControlButtonContainer className='wrong'>
+				<CloseIcon fontSize='inherit' color='inherit' />
+			</ControlButtonContainer>
 		</StyledDraft>
 	)
 }
